3a874413bebf96d12079db6a5b601766a36e9461
amrelkhodary <amrelkhodarybusiness@gmail.com>
2026-05-26 10:17:10 +0300

    added linked definition for binary tree

---
+++ binary-tree-linked.c
@@ -0,0 +1,14 @@
+/*
+ * This program implemets the binary tree data structure using a linked list.
+ */
+#include <stdlib.h>
+
+typedef struct node{
+    struct node* left;  //left subtree
+    int val;  //value
+    struct node* right;  //right subtree
+} node;
+
+int main(int argc, char** argv) {
+    return 0;
+}
